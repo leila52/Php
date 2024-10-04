501a43da7541f5f8d30fff8ee66be64d882d8813
leila52 <lailahaddad385@gmail.com>
2024-10-04 14:10:39 +0200

    mmmmm

--- adivina.php
+++ adivina.php
@@ -4,17 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adivina el numero xula</title>
+    
 </head>
 <body>
-    <?php
-        //campo hidden para guardar el numero aleatorio
-        define('TOPE',50)
-        $numAle=rand(1,TOPE);
+<?php
+        define('TOPE', 50); 
+            $numaAle = intval($_POST['numaAle']);
+            $intentos = intval($_POST['intentos']) + 1; 
+            $usuario = intval($_POST['usuario']);
+            if ($usuario > $numaAle) {
+                $mensaje = "el numero es menor que $adivinado. Llevas $intentos intentos.";
+            } elseif ($usuario < $numaAle) {
+                $mensaje = "el numero es mayor que $adivinado. Llevas $intentos intentos.";
+            } else {
+                $mensaje = "oleeeee has adivinado el nnmero en $intentos intentos.";
+                $numaAle= rand(1, TOPE); 
+                $intentos = 0; 
+            }
+        
     ?>
+    <h1>Adivinaaaaaaaa</h1>
+    <p><?php echo $mensaje; ?></p>
     <form action="formulario.php" method="post">
-        <label for="numero">Introduce el mu,ero:</label>
-        <input type="number" id="numero" name="numero"><br>
-        <button type="submit">comprobar</button>
+        <label for="numero">Introduce el número:</label>
+        <input type="usuario" id="usuario" name="usuario" required><br>
+        <br>
+        <input type="hidden" name="numaAle" value="<?php echo $numaAle; ?>">
+        <input type="hidden" name="intentos" value="<?php echo $intentos; ?>">
+        <button type="submit">enviar</button>
     </form>
 </body>
 </html>
